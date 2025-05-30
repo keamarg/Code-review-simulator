100169d16b6ce4383fcca40adb38e2c873632865
Martin Gundtoft <marg@kea.dk>
2025-05-30 15:26:48 +0200

    updated api key for supabase to vercel

--- src/exam-simulator/config/supabaseClient.tsx
+++ src/exam-simulator/config/supabaseClient.tsx
@@ -1,9 +1,16 @@
 import { createClient } from "@supabase/supabase-js";
 
+const apiKeyResponse = await fetch(
+  "https://api-key-server-codereview.vercel.app/api/database"
+);
+if (!apiKeyResponse.ok) {
+  throw new Error("Failed to fetch API key");
+}
+
+const apiKey = await apiKeyResponse.json();
+
 export const supabase = createClient(
   "https://gzoltpvnxwjoeycomcby.supabase.co",
 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd6b2x0cHZueHdqb2V5Y29tY2J5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDg1ODIxNjgsImV4cCI6MjA2NDE1ODE2OH0.BwW_I5QvYwzNpu8oF5y4GisYC9WHSKWbAIW9kOQKv5w"
-  /*"https://eqertgbvnnyyqqytvxjt.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVxZXJ0Z2J2bm55eXFxeXR2eGp0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU5MTMxNjIsImV4cCI6MjA2MTQ4OTE2Mn0.PPY7wW9U02XmNySowkdxQnh_R5jnvjEouVEBakowpIY"*/
+  apiKey
 );
